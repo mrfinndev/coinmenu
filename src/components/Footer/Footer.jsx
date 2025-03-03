56aa0cec0ee40a96acabc60021d271110ff25ca5
Finn <145452314+finntechnology@users.noreply.github.com>
2025-03-03 13:05:39 +0100

    style: update copyright text in Footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,9 @@
-import React from "react";
 import "./Footer.css";
 
 const Footer = () => {
   return (
     <div className="footer">
-      <p>Copyright @ 2024, Cryptoplace - All Rights Reserved.</p>
+      <p>Copyright @ 2024 Coinmenu | All Rights Reserved.</p>
     </div>
   );
 };
